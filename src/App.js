1bd35ce5cc3c31d654ab7ffc2c47aa65d83076ed
RaviKumar Kannan <ravikumar.kannan@xebia.com>
2022-07-29 14:17:46 +0530

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer">
-					Learn React---12
+					Learn React---125
 				</a>
 			</header>
 		</div>
